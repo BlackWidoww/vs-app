8ba83a7e8e45eb1b574c154376d06e69cc21b08a
BlackWidoww <courtney.shelley@gmail.com>
2014-09-18 13:27:22 -0700

    Update ui.R
    Deployable on MacBook, still not rendering on shinyapps.io

--- ui.R
+++ ui.R
@@ -1,51 +1,22 @@
 # ui.R
 
-###### LOAD DATA ######
-data <- read.csv("data/vesicular_stomatitis_2014.csv", header=TRUE)
-library(lubridate)
-data$DATE<-as.Date(data$DATE, "%m/%d/%Y")     # Convert dates to class "Date"
-
-## Create new column of outbreak duration
-DURATION<-as.numeric(data$DATE-data$DATE[1]) 
-data<-data.frame(data,DURATION)
-
-
 shinyUI(fluidPage(
   tags$head(
     tags$style("body {background-color: WhiteSmoke; }")),
   titlePanel(h1("Equine Vesicular Stomatitis", style="color: #61300D", align="center")),
-  h3("Outbreaks in the US 2004-2012", style = "color:#6F370F", align="center"),
-  br(),
-  br(),
-  h3("Background Information", style = "color:#7D3E11"),
-  p("Vesicular stomatitis virus (VSV) is an acute viral disease of cattle, horses, deer, and pigs (and occassionally
-    humans).  Phlebotomine sandflies are known vectors of VSV, and VSV can also be transmitted by direct contact between
-    infected animals.  Outbreaks in the U.S. generally are found to originate with the movement of animals (or insect vectors) from Mexico.
-    Infected animals present with fever 24-48 after infection, inflammatory lesions around the mouth, and excessive 
-    salivation due to rupture of lesions.  Outbreaks are controlled by quarantine, restriction of animal movement
-    and insect control.  There is no commercially available VSV vaccines."),
-  br(), 
+
   p("Vesicular stomatitis outbreaks in the U.S. are monitored and publicly reported by the U.S. Department of Agriculture (USDA) Animal and Plant Health 
-    Inspection Service."),
-  br(),
-  helpText(  "Situation reports for current and historical outbreaks are available",
+    Inspection Service.  Situation reports for current and historical outbreaks are available",
              a("here", href="http://1.usa.gov/1r2bsbJ", target="_blank")
   ),
-  
-  br(),
-  br(),
-  
-  h3("Geographic Outbreak Patterns By Year", style = "color:#8B4513"),
-  
-  
   
   sidebarLayout(
     
     sidebarPanel(
       selectInput("year", 
                   label = h5("Select Year", style="color:#97582B"), 
-                  choices = c("2014", "2012", "2010", "2009", "2006", "2005", "2004"),
-                  selected = "2014"),
+                  choices = c("Overview", "2014", "2012", "2010", "2009", "2006", "2005", "2004"),
+                  selected = "Overview"),
       br(),
       sliderInput("days", 
                   label = h5("Days since Initial Case:", style="color:#A26A42"),
@@ -67,10 +38,12 @@ shinyUI(fluidPage(
     
     mainPanel(
       tabsetPanel(
-        tabPanel("Map",
+        tabPanel("Overview",
                  h3(textOutput("text1"), style="color:#A6790A", align="center"),
                  h5(textOutput("text2"), style="color:#B8860B", align="center"),
-                 em(textOutput("text3"), style = "color:OliveDrab"),
+                 textOutput("text3")
+                 ),
+        tabPanel("Map",
                  plotOutput("map"),
                  textOutput("text4")
         ),
